59e2f18f4fc689918010ffd5328202cd596a11f9
benicio <beniciobrandao@hotmail.com>
2025-04-15 12:41:27 -0300

    Cria pasta Entities e as classes das entidades

---
+++ BloodDonationSystem.Application/Entities/Address.cs
@@ -0,0 +1,19 @@
+﻿namespace BloodDonationSystem.Application.Entities;
+public class Address
+{
+    public Address(int donorId, string street, string city, string state, string cep)
+    {
+        DonorId = donorId;
+        Street = street;
+        City = city;
+        State = state;
+        Cep = cep;
+    }
+    public int Id { get; private set; }
+    public int DonorId {  get; private set; }
+    public string Street {  get; private set; }
+    public string City { get; private set; }
+    public string State {  get; private set; }
+    public string Cep {  get; private set; }
+    public Donor? Donor { get; private set; }
+}
