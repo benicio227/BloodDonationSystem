db96faace73491df38c8ac8f07bb62a77a216630
benicio <beniciobrandao@hotmail.com>
2025-04-15 14:00:03 -0300

    Cria a pasta models e classes para as ViewModels

---
+++ BloodDonationSystem.Application/Models/DonorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Application.Models;
+public class DonorViewModel
+{
+    public DonorViewModel(int id, string fullName, string email, DateTime birthDate, string gender, double weight, string bloodType, string rgFactor)
+    {
+        FullName = fullName;
+        Email = email;
+        BirthDate = birthDate;
+        Gender = gender;
+        Weight = weight;
+        BloodType = bloodType;
+    }
+    public int Id { get; private set; }
+    public string FullName { get; private set; }
+    public string Email { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public string Gender { get; private set; }
+    public double Weight { get; private set; }
+    public string BloodType { get; private set; }
+    public string RgFactor { get; private set; }
+
+    public DonorViewModel FromEntity(Donor donor)
+    {
+        return new DonorViewModel(donor.Id, donor.FullName, donor.Email, donor.BirthDate, donor.Gender, donor.Weight, donor.BloodType, donor.RgFactor);
+    }
+}
