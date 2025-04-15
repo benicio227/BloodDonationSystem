847085261cab12d0ee4d587e5a7152082a05e18e
benicio <beniciobrandao@hotmail.com>
2025-04-15 14:46:53 -0300

    Cria a pasta Repositories para acesso a dados no banco

---
+++ BloodDonationSystem.Infrastucture/Repositories/DonorRepository.cs
@@ -0,0 +1,20 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Core.Repositories;
+using BloodDonationSystem.Infrastucture.Persistence;
+
+namespace BloodDonationSystem.Infrastucture.Repositories;
+public class DonorRepository : IDonorRepository
+{
+    private readonly BloodDonationSystemDbContext _context;
+    public DonorRepository(BloodDonationSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Donor> Add(Donor donor)
+    {
+        await _context.AddAsync(donor);
+        await _context.SaveChangesAsync();
+
+        return donor;
+    }
+}
