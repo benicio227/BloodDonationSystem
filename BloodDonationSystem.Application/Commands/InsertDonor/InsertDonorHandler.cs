919c934a968acc9f5020830ca19a54752506adab
benicio <beniciobrandao@hotmail.com>
2025-04-15 14:46:05 -0300

    Cria e define o método Handler para a entidade Donor

---
+++ BloodDonationSystem.Application/Commands/InsertDonor/InsertDonorHandler.cs
@@ -0,0 +1,24 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertDonor;
+public class InsertDonorHandler : IRequestHandler<InsertDonorCommand, DonorViewModel>
+{
+    private readonly IDonorRepository _repository;
+
+    public InsertDonorHandler(IDonorRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<DonorViewModel> Handle(InsertDonorCommand request, CancellationToken cancellationToken)
+    {
+        var donor = request.ToEntity();
+
+        var donorExist = await _repository.Add(donor);
+
+        var model = DonorViewModel.FromEntity(donor);
+
+        return model;
+    }
+}
