4a8f0e495f7406b56458aed86e3c0113b4057c56
benicio <beniciobrandao@hotmail.com>
2025-04-16 18:02:12 -0300

    Controller de Address implementado

---
+++ BloodDonationSystem.Api/Controllers/AddressController.cs
@@ -0,0 +1,48 @@
+﻿using BloodDonationSystem.Application.Commands.InsertAddress;
+using BloodDonationSystem.Application.Queries.DeleteAddressById;
+using BloodDonationSystem.Application.Queries.GetAddressById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonationSystem.Api.Controllers;
+[Route("api/donors/{donorId}/address")]
+[ApiController]
+public class AddressController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AddressController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(int donorId, InsertAddressCommand command)
+    {
+        command.DonorId = donorId;
+
+        var result = await _mediator.Send(command);
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById(int donorId)
+    {
+        var query = new GetAddressByIdQuery(donorId);
+
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int donorId)
+    {
+        var query = new DeleteAddressByIdQuery(donorId);
+
+        await _mediator.Send(query);
+
+        return Ok();
+    }
+}
