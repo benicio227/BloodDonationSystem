22c1cba8782a28f86d1baa2ec8b5b9171b3430fb
benicio <beniciobrandao@hotmail.com>
2025-04-16 18:35:59 -0300

    Cria interface para entidade Donation

---
+++ BloodDonationSystem.Core/Repositories/IDonationRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Core.Repositories;
+public interface IDonationRepository
+{
+    public Task<Donation?> Add(Donation donation);
+    public Task<Donation?> GetById(int id);
+    public Task Delete(int id);
+}
