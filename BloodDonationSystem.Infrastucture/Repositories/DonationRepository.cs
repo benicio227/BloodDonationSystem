5d5204ac7933fcd9a30b0a3fcd3cc32eb54e4aa7
benicio <beniciobrandao@hotmail.com>
2025-04-16 18:35:15 -0300

    Cria repositorio para acesso a dados da entidade Donation

---
+++ BloodDonationSystem.Infrastucture/Repositories/DonationRepository.cs
@@ -0,0 +1,37 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Core.Repositories;
+using BloodDonationSystem.Infrastucture.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationSystem.Infrastucture.Repositories;
+public class DonationRepository : IDonationRepository
+{
+    private readonly BloodDonationSystemDbContext _context;
+
+    public DonationRepository(BloodDonationSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<Donation?> Add(Donation donation)
+    {
+        await _context.AddAsync(donation);
+        await _context.SaveChangesAsync();
+
+        return donation;
+    }
+
+    public async Task<Donation?> GetById(int id)
+    {
+        var donation = await _context.Donations.FirstOrDefaultAsync(d => d.DonorId == id);
+
+        return donation;
+    }
+    public async Task Delete(int id)
+    {
+        var donation = await _context.Donations.FirstOrDefaultAsync(d => d.DonorId == id);
+
+        _context.Donations.Remove(donation!);
+        await _context.SaveChangesAsync();
+    }
+
+}
