2a96849fe99b365d6d06a7fb0b4e569ede38597d
benicio <beniciobrandao@hotmail.com>
2025-04-16 16:26:51 -0300

    Cria a pasta InsertAddress e define o Handle para a entidade Address

---
+++ BloodDonationSystem.Application/Commands/InsertAddress/InsertAddressCommand.cs
@@ -0,0 +1,18 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertAddress;
+public class InsertAddressCommand : IRequest<AddressViewModel>
+{
+    public int DonorId { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Cep { get; set; }
+
+    public Address ToEntity()
+    {
+        return new Address(DonorId, Street, City, State, Cep);
+    }
+}
---
+++ BloodDonationSystem.Application/Commands/InsertAddress/InsertAddressHandler.cs
@@ -0,0 +1,24 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertAddress;
+public class InsertAddressHandler : IRequestHandler<InsertAddressCommand, AddressViewModel>
+{
+    private readonly IAddressRepository _repository;
+
+    public InsertAddressHandler(IAddressRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<AddressViewModel> Handle(InsertAddressCommand request, CancellationToken cancellationToken)
+    {
+        var address = request.ToEntity();
+
+        var addressExist = await _repository.Add(address);
+
+        var model = AddressViewModel.FromEntity(address);
+
+        return model;
+    }
+}
