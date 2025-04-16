f1509dbf5e76f2d28b9fe05d383278fb61811971
benicio <beniciobrandao@hotmail.com>
2025-04-16 14:09:10 -0300

    Cria interface para entidade Address

---
+++ BloodDonationSystem.Core/Repositories/IAddressRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Core.Repositories;
+public interface IAddressRepository
+{
+    public Task<Address?> Add(Address address);
+    public Task<Address?> GetById(int id);
+    public Task Delete(int id);
+}
