bb3ced67d77624b65e428078a87501a11bbb4c86
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:37:05 -0300

    Cria a ViewModel para apresentar no front

---
+++ BloodDonationSystem.Application/Models/BloodStockViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Application.Models;
+public class BloodStockViewModel 
+{
+    public BloodStockViewModel(int id, string bloodType, string rgFactor, int amountMl)
+    {
+        Id = id;
+        BloodType = bloodType;
+        RgFactor = rgFactor;
+        AmountMl = amountMl;
+    }
+    public int Id { get; private set; }
+    public string BloodType { get; private set; }
+    public string RgFactor { get; private set; }
+    public int AmountMl { get; private set; }
+
+    public static BloodStockViewModel FromEntity(BloodStock bloodStock)
+    {
+        return new BloodStockViewModel(bloodStock.Id, bloodStock.BloodType, bloodStock.RgFactor, bloodStock.AmountMl);
+    }
+}
