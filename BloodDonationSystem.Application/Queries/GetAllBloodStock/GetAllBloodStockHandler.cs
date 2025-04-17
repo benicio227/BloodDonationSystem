26b7796973795a7fb0cfcf1918c3a2683548abe2
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:39:45 -0300

    Cria a pasta GetAllBloodStock para buscar todos os estoques do banco

---
+++ BloodDonationSystem.Application/Queries/GetAllBloodStock/GetAllBloodStockHandler.cs
@@ -0,0 +1,22 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetAllBloodStock;
+public class GetAllBloodStockHandler : IRequestHandler<GetAllBloodStockQuery, List<BloodStockViewModel>>
+{
+    private readonly IBloodStockRepository _repository;
+
+    public GetAllBloodStockHandler(IBloodStockRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<List<BloodStockViewModel>> Handle(GetAllBloodStockQuery request, CancellationToken cancellationToken)
+    {
+        var bloodStocks = await _repository.GetAll();
+
+        var bloodStockViewModel = bloodStocks.Select(bloodStock => BloodStockViewModel.FromEntity(bloodStock)).ToList(); ;
+
+        return bloodStockViewModel;
+    }
+}
---
+++ BloodDonationSystem.Application/Queries/GetAllBloodStock/GetAllBloodStockQuery.cs
@@ -0,0 +1,7 @@
+﻿using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetAllBloodStock;
+public class GetAllBloodStockQuery : IRequest<List<BloodStockViewModel>>
+{
+}
