35b61fcaafc6f668f8187e08080b6afac7db679e
benicio <beniciobrandao@hotmail.com>
2025-04-16 22:46:58 -0300

    Cria a pasta InsertDonation e a classe Handler

---
+++ BloodDonationSystem.Application/Commands/InsertDonation/InsertDonationCommand.cs
@@ -0,0 +1,16 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertDonation;
+public class InsertDonationCommand : IRequest<DonationViewModel>
+{
+    public int DonorId { get; set; }
+    public int AmountMl { get; set; }
+
+
+    public Donation ToEntity()
+    {
+        return new Donation(DonorId, AmountMl);
+    }
+}
---
+++ BloodDonationSystem.Application/Commands/InsertDonation/InsertDonationHandler.cs
@@ -0,0 +1,24 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertDonation;
+public class InsertDonationHandler : IRequestHandler<InsertDonationCommand, DonationViewModel>
+{
+    private readonly IDonationRepository _repository;
+
+    public InsertDonationHandler(IDonationRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<DonationViewModel> Handle(InsertDonationCommand request, CancellationToken cancellationToken)
+    {
+        var donation = request.ToEntity();
+
+        await _repository.Add(donation);
+
+        var model = DonationViewModel.FromEntity(donation);
+
+        return model;
+    }
+}
