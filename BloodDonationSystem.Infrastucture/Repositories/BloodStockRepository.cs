19be7c88f793101d8eb72fcae5d18acc06735aba
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:37:58 -0300

    Cria o repositório para acesso a dados no banco de dados

---
+++ BloodDonationSystem.Infrastucture/Repositories/BloodStockRepository.cs
@@ -0,0 +1,37 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Core.Repositories;
+using BloodDonationSystem.Infrastucture.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationSystem.Infrastucture.Repositories;
+public class BloodStockRepository : IBloodStockRepository
+{
+    private readonly BloodDonationSystemDbContext _context;
+
+    public BloodStockRepository(BloodDonationSystemDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<BloodStock> Add(BloodStock bloodStock)
+    {
+        await _context.bloodStocks.AddAsync(bloodStock);
+        await _context.SaveChangesAsync();
+
+        return bloodStock;
+    }
+
+    public async Task<List<BloodStock>> GetAll()
+    {
+        var bloodStocks = await _context.bloodStocks.ToListAsync();
+
+        return bloodStocks;
+    }
+
+    public async Task Delete(int id)
+    {
+        var bloodStock = await _context.bloodStocks.FirstOrDefaultAsync(b => b.Id == id);
+
+        _context.bloodStocks.Remove(bloodStock!);
+        await _context.SaveChangesAsync();
+    }
+}
