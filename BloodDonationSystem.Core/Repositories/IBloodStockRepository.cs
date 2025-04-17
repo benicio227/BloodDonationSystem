f2a9401c83530e6226bd30a0eb447a0302c5aa43
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:38:50 -0300

    Cria a interface para BloodStockRepository

---
+++ BloodDonationSystem.Core/Repositories/IBloodStockRepository.cs
@@ -0,0 +1,9 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Core.Repositories;
+public interface IBloodStockRepository
+{
+    public Task<BloodStock> Add(BloodStock bloodStock);
+    public Task<List<BloodStock>> GetAll();
+    public Task Delete(int id);
+}
