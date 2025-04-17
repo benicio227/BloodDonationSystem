d307b94e120ae4c82bbaae4e26c296b5324e20e4
benicio <beniciobrandao@hotmail.com>
2025-04-16 22:50:19 -0300

    Cria a pasta GetDonationById e o método Handler

---
+++ BloodDonationSystem.Application/Queries/GetDonationById/GetDonationByIdHandler.cs
@@ -0,0 +1,22 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetDonationById;
+public class GetDonationByIdHandler : IRequestHandler<GetDonationByIdQuery, DonationViewModel>
+{
+    private readonly IDonationRepository _repository;
+
+    public GetDonationByIdHandler(IDonationRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<DonationViewModel> Handle(GetDonationByIdQuery request, CancellationToken cancellationToken)
+    {
+        var donation = await _repository.GetById(request.Id);
+
+        var model = DonationViewModel.FromEntity(donation);
+
+        return model;
+    }
+}
---
+++ BloodDonationSystem.Application/Queries/GetDonationById/GetDonationByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using BloodDonationSystem.Application.Models;
+using MediatR;
+using System.Runtime.InteropServices;
+
+namespace BloodDonationSystem.Application.Queries.GetDonationById;
+public class GetDonationByIdQuery : IRequest<DonationViewModel>
+{
+    public GetDonationByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
