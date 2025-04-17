44e8686dba337d4896b0c4206c5b330a22b71ea8
benicio <beniciobrandao@hotmail.com>
2025-04-16 22:45:37 -0300

    Cria o controller e as Actions da entidade Donation

---
+++ BloodDonationSystem.Api/Controllers/DonationController.cs
@@ -0,0 +1,48 @@
+﻿using BloodDonationSystem.Application.Commands.InsertDonation;
+using BloodDonationSystem.Application.Queries.DeleteDonationById;
+using BloodDonationSystem.Application.Queries.GetDonationById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonationSystem.Api.Controllers;
+[Route("api/donors/{donorId}/donations")]
+[ApiController]
+public class DonationController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DonationController(IMediator mediator)
+    {
+        _mediator = mediator;   
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(int donorId, InsertDonationCommand command)
+    {
+        command.DonorId = donorId;
+
+        var result = await _mediator.Send(command);
+
+        return Created(string.Empty, result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetById(int donorId)
+    {
+        var query = new GetDonationByIdQuery(donorId);
+
+        var result = await _mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int donorId)
+    {
+        var query = new DeleteDonationByIdQuery(donorId);
+
+        var result = await _mediator.Send(query);
+
+        return NoContent();
+    }
+}
