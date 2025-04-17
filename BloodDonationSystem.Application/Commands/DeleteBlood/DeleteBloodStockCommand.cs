d472e8f4a6cccc541c018a6294ca294ab2a4dc88
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:36:00 -0300

    Cria pasta DeleteBlood e o Handle

---
+++ BloodDonationSystem.Application/Commands/DeleteBlood/DeleteBloodStockCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.DeleteBlood;
+public class DeleteBloodStockCommand : IRequest<Unit>
+{
+    public DeleteBloodStockCommand(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
---
+++ BloodDonationSystem.Application/Commands/DeleteBlood/DeleteBloodStockHanlder.cs
@@ -0,0 +1,19 @@
+﻿using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.DeleteBlood;
+public class DeleteBloodStockHanlder : IRequestHandler<DeleteBloodStockCommand, Unit>
+{
+    private readonly IBloodStockRepository _repository;
+
+    public DeleteBloodStockHanlder(IBloodStockRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(DeleteBloodStockCommand request, CancellationToken cancellationToken)
+    {
+        await _repository.Delete(request.Id);
+
+        return Unit.Value;
+    }
+}
