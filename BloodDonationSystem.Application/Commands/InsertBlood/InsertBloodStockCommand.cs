34ed30b62720ec8f0caa44f7472eab7b6a71b9f7
benicio <beniciobrandao@hotmail.com>
2025-04-17 13:40:40 -0300

    Cria pasta InsertBlood para registrar um novo estoque

---
+++ BloodDonationSystem.Application/Commands/InsertBlood/InsertBloodStockCommand.cs
@@ -0,0 +1,16 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertBlood;
+public class InsertBloodStockCommand : IRequest<BloodStockViewModel>
+{
+    public string BloodType { get; set; }
+    public string RgFactor { get; set; }
+    public int AmountMl { get; set; }
+
+    public BloodStock ToEntity()
+    {
+        return new BloodStock(BloodType, RgFactor, AmountMl);
+    }
+}
---
+++ BloodDonationSystem.Application/Commands/InsertBlood/InsertBloodStockHandler.cs
@@ -0,0 +1,24 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertBlood;
+public class InsertBloodStockHandler : IRequestHandler<InsertBloodStockCommand, BloodStockViewModel>
+{
+    private readonly IBloodStockRepository _repsoitory;
+
+    public InsertBloodStockHandler(IBloodStockRepository repository)
+    {
+        _repsoitory = repository;
+    }
+    public async Task<BloodStockViewModel> Handle(InsertBloodStockCommand request, CancellationToken cancellationToken)
+    {
+        var bloodStock = request.ToEntity();
+
+        var bloodExist = await _repsoitory.Add(bloodStock);
+
+        var model = BloodStockViewModel.FromEntity(bloodStock);
+
+        return model;
+    }
+}
