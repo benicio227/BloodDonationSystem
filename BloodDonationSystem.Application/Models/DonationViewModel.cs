12b694920a1735285c47b79dc54b12d31304fc7f
benicio <beniciobrandao@hotmail.com>
2025-04-16 22:47:49 -0300

    Cria a classe DonationViewModel para ser exibida como resposta

---
+++ BloodDonationSystem.Application/Models/DonationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Application.Models;
+public class DonationViewModel
+{
+    public DonationViewModel(int id, int donorId, int amountMl)
+    {
+        Id = id;
+        DonorId = donorId;
+        AmountMl = amountMl;
+        DonationDate = DateTime.Now;
+    }
+    public int Id { get; private set; }
+    public int DonorId { get; private set; }
+    public DateTime DonationDate { get; private set; }
+    public int AmountMl { get; private set; }
+    public Donor? Donor { get; private set; }
+
+    public static DonationViewModel FromEntity(Donation donation)
+    {
+        return new DonationViewModel(donation.Id, donation.DonorId, donation.AmountMl);
+    }
+}
