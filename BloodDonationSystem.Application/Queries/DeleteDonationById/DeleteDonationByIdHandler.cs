f1911615bf4a4e055f26f49b8f43ec90ab3ec4b1
benicio <beniciobrandao@hotmail.com>
2025-04-16 22:49:21 -0300

    Cria a pasta DeleteDonationById e implementa o método Handler

---
+++ BloodDonationSystem.Application/Queries/DeleteDonationById/DeleteDonationByIdHandler.cs
@@ -0,0 +1,19 @@
+﻿using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.DeleteDonationById;
+public class DeleteDonationByIdHandler : IRequestHandler<DeleteDonationByIdQuery, Unit>
+{
+    private readonly IDonationRepository _repository;
+
+    public DeleteDonationByIdHandler(IDonationRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<Unit> Handle(DeleteDonationByIdQuery request, CancellationToken cancellationToken)
+    {
+        await _repository.Delete(request.Id);
+
+        return Unit.Value;
+    }
+}
---
+++ BloodDonationSystem.Application/Queries/DeleteDonationById/DeleteDonationByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.DeleteDonationById;
+public class DeleteDonationByIdQuery : IRequest<Unit>
+{
+    public DeleteDonationByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
