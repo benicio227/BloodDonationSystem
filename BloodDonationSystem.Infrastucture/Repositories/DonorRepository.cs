307d3863970c04cb2002ca11bfd36ae195db014c
benicio <beniciobrandao@hotmail.com>
2025-04-18 07:45:28 -0300

    Implementa o método GetById para acesso ao banco de dados

--- BloodDonationSystem.Infrastucture/Repositories/DonorRepository.cs
+++ BloodDonationSystem.Infrastucture/Repositories/DonorRepository.cs
@@ -1,6 +1,7 @@
 ﻿using BloodDonationSystem.Application.Entities;
 using BloodDonationSystem.Core.Repositories;
 using BloodDonationSystem.Infrastucture.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace BloodDonationSystem.Infrastucture.Repositories;
 public class DonorRepository : IDonorRepository
@@ -14,6 +15,13 @@ public class DonorRepository : IDonorRepository
     {
         await _context.AddAsync(donor);
         await _context.SaveChangesAsync();
+
+        return donor;
+    }
+
+    public async Task<Donor?> GetById(int id)
+    {
+        var donor = await _context.Donors.FirstOrDefaultAsync(d => d.Id == id);
 
         return donor;
     }
