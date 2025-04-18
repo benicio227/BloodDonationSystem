7f9bc430ac2933eb45968eb7a499f442b1779f2c
benicio <beniciobrandao@hotmail.com>
2025-04-18 10:55:23 -0300

    Implementa regra de negócio para verificar se doador é maior de idade

--- BloodDonationSystem.Application/Commands/InsertDonation/InsertDonationHandler.cs
+++ BloodDonationSystem.Application/Commands/InsertDonation/InsertDonationHandler.cs
@@ -6,13 +6,23 @@ namespace BloodDonationSystem.Application.Commands.InsertDonation;
 public class InsertDonationHandler : IRequestHandler<InsertDonationCommand, DonationViewModel>
 {
     private readonly IDonationRepository _repository;
-
-    public InsertDonationHandler(IDonationRepository repository)
+    private readonly IDonorRepository _donorRepository;
+    public InsertDonationHandler(IDonationRepository repository, IDonorRepository donorRepository)
     {
         _repository = repository;
+        _donorRepository = donorRepository;
     }
     public async Task<DonationViewModel> Handle(InsertDonationCommand request, CancellationToken cancellationToken)
     {
+        var donor = await _donorRepository.GetById(request.DonorId);
+
+        var data18Year = donor.BirthDate.AddYears(18);
+
+        if (DateTime.Today < data18Year)
+        {
+            throw new Exception("O doador precisa ter 18 anos ou mais para doar sangue.");
+        }
+
         var donation = request.ToEntity();
 
         await _repository.Add(donation);
