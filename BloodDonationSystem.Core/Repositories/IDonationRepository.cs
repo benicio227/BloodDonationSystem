b6ca6f69689889add1d70ec00003a20e3746faf1
benicio <beniciobrandao@hotmail.com>
2025-04-18 12:52:47 -0300

    Cria método para a interface IDonationRepository

--- BloodDonationSystem.Core/Repositories/IDonationRepository.cs
+++ BloodDonationSystem.Core/Repositories/IDonationRepository.cs
@@ -5,5 +5,6 @@ public interface IDonationRepository
 {
     public Task<Donation?> Add(Donation donation);
     public Task<Donation?> GetById(int id);
+    public Task<Donation?> GetLastDonationByDonorId(int id);
     public Task Delete(int id);
 }
