ebba7933fe40f117540085e46e8b08b7b4d6ef9a
benicio <beniciobrandao@hotmail.com>
2025-04-19 07:37:35 -0300

    Cria pasta GetBloodStockById e o Handler para buscar um estoque pelo ID

---
+++ BloodDonationSystem.Application/Queries/GetBloodStockById/GetBloodStockByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetBloodStockById;
+public class GetBloodStockByIdHandler : IRequestHandler<GetBloodStockByIdQuery, ResultViewModel<BloodStockViewModel>>
+{
+    private readonly IBloodStockRepository _repository;
+
+    public GetBloodStockByIdHandler(IBloodStockRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<ResultViewModel<BloodStockViewModel>> Handle(GetBloodStockByIdQuery request, CancellationToken cancellationToken)
+    {
+        var bloodStock = await _repository.GetById(request.Id);
+
+        if (bloodStock is null)
+        {
+            return ResultViewModel<BloodStockViewModel>.Error("Estoque não encontrado com o ID fornecido.");
+        }
+
+        var model = BloodStockViewModel.FromEntity(bloodStock);
+
+        return ResultViewModel<BloodStockViewModel>.Success(model);
+    }
+}
---
+++ BloodDonationSystem.Application/Queries/GetBloodStockById/GetBloodStockByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetBloodStockById;
+public class GetBloodStockByIdQuery : IRequest<ResultViewModel<BloodStockViewModel>>
+{
+    public GetBloodStockByIdQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
