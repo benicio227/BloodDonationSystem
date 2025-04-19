3df9e695770de721ffca5f45b250ecf54798ec49
benicio <beniciobrandao@hotmail.com>
2025-04-19 15:42:59 -0300

    Adiciona propriedade MinimumAmounMml para controlar quantidade mínima de estoque

---
+++ BloodDonationSystem.Infrastucture/Persistence/Migrations/20250419183932_AdicionandoPropriedade.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BloodDonationSystem.Infrastucture.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoPropriedade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumAmountMl",
+                table: "bloodStocks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumAmountMl",
+                table: "bloodStocks");
+        }
+    }
+}
