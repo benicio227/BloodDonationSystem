dbd72409108e85a3668304d48d3c28eb221416ff
benicio <beniciobrandao@hotmail.com>
2025-04-21 20:21:55 -0300

    Cria a ViewModel para exibir as propriedades atualizadas do doador

---
+++ BloodDonationSystem.Application/Models/UpdateDonorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Application.Models;
+public class UpdateDonorViewModel
+{
+    public UpdateDonorViewModel(string email, double weight, string bloodType, string rgFactor)
+    {
+        Email = email;
+        Weight = weight;
+        BloodType = bloodType;
+        RgFactor = rgFactor;
+    }
+
+    public string Email { get; private set; }
+    public double Weight { get; private set; }
+    public string BloodType { get; private set; }
+    public string RgFactor { get; private set; }
+
+    public UpdateDonorViewModel FromEntity(Donor donor)
+    {
+        return new UpdateDonorViewModel(donor.Email, donor.Weight, donor.BloodType, donor.RgFactor);
+    }
+}
