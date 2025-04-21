3aae88205023fcf26bc653be6b750089f15ab8a2
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:17:30 -0300

    Adiciona método da interface para buscar todas as doações de um doador

--- BloodDonationSystem.Core/Repositories/IDonationRepository.cs
+++ BloodDonationSystem.Core/Repositories/IDonationRepository.cs
@@ -5,6 +5,7 @@ public interface IDonationRepository
 {
     public Task<Donation?> Add(Donation donation);
     public Task<Donation?> GetById(int id);
+    public Task<List<Donation?>> GetAll(int id);
     public Task<Donation?> GetLastDonationByDonorId(int id);
     public Task<Donation?> Delete(int id);
 }
