6e1a8b7771e1a0cfa44a929a6fc1c5599e9a3c6d
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:08:33 -0300

    Cria a classe ViaCepViewModel para receber os dados da Api externa para cadastrar endereço

---
+++ BloodDonationSystem.Application/Models/ViaCepViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BloodDonationSystem.Application.Models;
+public class ViaCepViewModel
+{
+    public string Logradouro { get; set; } = string.Empty;
+    public string Localidade { get; set; } = string.Empty;
+    public string Uf { get; set; } = string.Empty;
+}
