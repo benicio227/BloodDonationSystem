f5a54ff3c23f35f2b718af47bd8a6851b61ee59f
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:28:58 -0300

    Utiliza injeção de dependência para acessar a classe da interface ICepService

--- BloodDonationSystem.Application/Commands/InsertAddress/InsertAddressHandler.cs
+++ BloodDonationSystem.Application/Commands/InsertAddress/InsertAddressHandler.cs
@@ -1,19 +1,50 @@
-﻿using BloodDonationSystem.Application.Models;
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
 using BloodDonationSystem.Core.Repositories;
+using BloodDonationSystem.Infrastucture.Cep;
 using MediatR;
+using Microsoft.AspNetCore.Http.Timeouts;
 
 namespace BloodDonationSystem.Application.Commands.InsertAddress;
 public class InsertAddressHandler : IRequestHandler<InsertAddressCommand, ResultViewModel<AddressViewModel>>
 {
     private readonly IAddressRepository _repository;
+    private readonly ICepService _cepService;
 
-    public InsertAddressHandler(IAddressRepository repository)
+    public InsertAddressHandler(IAddressRepository repository, ICepService cepService)
     {
         _repository = repository;
+        _cepService = cepService;
     }
     public async Task<ResultViewModel<AddressViewModel>> Handle(InsertAddressCommand request, CancellationToken cancellationToken)
     {
-        var address = request.ToEntity();
+
+        var cepInfo = await _cepService.ConsultarCepAscyn(request.Cep);
+
+        if (cepInfo is null)
+        {
+            return ResultViewModel<AddressViewModel>.Error("CEP inválido ou não encontrado.");
+        }
+
+        var street = !string.IsNullOrWhiteSpace(cepInfo.Logradouro)
+            ?cepInfo.Logradouro
+            : string.IsNullOrWhiteSpace(request.Street) || request.Street!.ToLower() == "string"
+                ? null
+                : request.Street;
+
+        if (string.IsNullOrWhiteSpace(street))
+        {
+            return ResultViewModel<AddressViewModel>.Error("Não foi possível encontrar o nome da rua.");
+        }
+
+        var address = new Address
+        (
+            request.DonorId,
+            street,
+            cepInfo.Localidade,
+            cepInfo.Uf,
+            request.Cep
+        );
 
         var addressExist = await _repository.Add(address);
 
