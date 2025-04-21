69c2a62b3542c8b557ea31222e18649ba8bfcaac
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:14:32 -0300

    Adiciona propriedade para controlar estoque

--- BloodDonationSystem.Core/Entities/BloodStock.cs
+++ BloodDonationSystem.Core/Entities/BloodStock.cs
@@ -1,11 +1,13 @@
 ﻿namespace BloodDonationSystem.Application.Entities;
 public class BloodStock
 {
-    public BloodStock(string bloodType, string rgFactor, int amountMl)
+    public BloodStock(string bloodType, string rgFactor, int amountMl, int minimumAmountMl)
     {
         BloodType = bloodType;
         RgFactor = rgFactor;
         AmountMl = amountMl;
+        MinimumAmountMl = minimumAmountMl;
+
     }
     public int Id { get; private set; }
     public string BloodType {  get; private set; }
