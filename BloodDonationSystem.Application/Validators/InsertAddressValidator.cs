526d7aa035581ceef3092bed0d6e861545a25f19
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:24:35 -0300

    Remove validações das propriedades desnecessárias do request

--- BloodDonationSystem.Application/Validators/InsertAddressValidator.cs
+++ BloodDonationSystem.Application/Validators/InsertAddressValidator.cs
@@ -10,18 +10,6 @@ public class InsertAddressValidator : AbstractValidator<InsertAddressCommand>
             .GreaterThan(0)
                 .WithMessage("O ID do doador deve ser maior que 0.");
 
-        RuleFor(a => a.Street)
-            .NotEmpty()
-                .WithMessage("O nome do endereço da rua é obrigatório.");
-
-        RuleFor(a => a.City)
-            .NotEmpty()
-                .WithMessage("O nome da cidade é obrigatório.");
-
-        RuleFor(a => a.State)
-            .NotEmpty()
-                .WithMessage("O nome do estado é obrigatório.");
-
         RuleFor(a => a.Cep)
             .NotEmpty()
                 .WithMessage("O CEP é obrigatório.");
