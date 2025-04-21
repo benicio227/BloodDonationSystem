8bd5c1b041373d9e72eb852c1d2fcc40cf15c0c0
benicio <beniciobrandao@hotmail.com>
2025-04-21 11:07:15 -0300

    Cria pasta GettAllDoantionById para buscar todas as doações associadas ao Doador

---
+++ BloodDonationSystem.Application/Queries/GetAllDonationById/GetAllDonationHandler.cs
@@ -0,0 +1,26 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Repositories;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetAllDonationById;
+public class GetAllDonationHandler : IRequestHandler<GetAllDonationQuery, ResultViewModel<List<Donation>>>
+{
+    private readonly IDonationRepository _repository;
+
+    public GetAllDonationHandler(IDonationRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<ResultViewModel<List<Donation>>> Handle(GetAllDonationQuery request, CancellationToken cancellationToken)
+    {
+        var donations = await _repository.GetAll(request.Id);
+
+        if (donations is null || !donations.Any())
+        {
+            return ResultViewModel<List<Donation>>.Error($"Nenhum ID {request.Id} encontrado.");
+        }
+
+        return ResultViewModel<List<Donation>>.Success(donations!);
+    }
+}
---
+++ BloodDonationSystem.Application/Queries/GetAllDonationById/GetAllDonationQuery.cs
@@ -0,0 +1,14 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Queries.GetAllDonationById;
+public class GetAllDonationQuery : IRequest<ResultViewModel<List<Donation>>>
+{
+    public GetAllDonationQuery(int id)
+    {
+        Id = id;
+    }
+
+    public int Id { get; set; }
+}
