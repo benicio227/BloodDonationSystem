9ba610511cad6fbea7e76b4375655cfb3ed7dc25
benicio <beniciobrandao@hotmail.com>
2025-04-22 10:50:22 -0300

    Adiciona métodos Delete e Restore para controle de deleção lógica

--- BloodDonationSystem.Core/Entities/BloodStock.cs
+++ BloodDonationSystem.Core/Entities/BloodStock.cs
@@ -7,6 +7,7 @@ public class BloodStock
         RgFactor = rgFactor;
         AmountMl = amountMl;
         MinimumAmountMl = minimumAmountMl;
+        IsDeleted = false;
 
     }
     public int Id { get; private set; }
@@ -14,9 +15,20 @@ public class BloodStock
     public string RgFactor {  get; private set; }
     public int AmountMl { get; private set; }
     public int MinimumAmountMl {  get; private set; }
+    public bool IsDeleted {  get; private set; }
     public void AddAmount(int amount)
     {
         AmountMl += amount;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
     }
 }
  
