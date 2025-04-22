d9812333bf213c49652776e04e05954fc7fa46f7
benicio <beniciobrandao@hotmail.com>
2025-04-22 08:33:02 -0300

    Adiciona método Update para ser implementado no repositorio

--- BloodDonationSystem.Core/Repositories/IAddressRepository.cs
+++ BloodDonationSystem.Core/Repositories/IAddressRepository.cs
@@ -6,4 +6,5 @@ public interface IAddressRepository
     public Task<Address?> Add(Address address);
     public Task<Address?> GetById(int id);
     public Task<Address?> Delete(int id);
+    public Task<Address?> Update(int id);
 }
