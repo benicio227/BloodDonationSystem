8089e55ff463d4e1a59d8e84b9e6e10522083a46
benicio <beniciobrandao@hotmail.com>
2025-04-22 08:31:32 -0300

    Adiciona métodos para atualizar propriedades e acrescenta métodos para realizar a deleção lógica da entidade Address

--- BloodDonationSystem.Core/Entities/Address.cs
+++ BloodDonationSystem.Core/Entities/Address.cs
@@ -8,6 +8,7 @@ public class Address
         City = city;
         State = state;
         Cep = cep;
+        IsDeleted = false;
     }
     public int Id { get; private set; }
     public int DonorId {  get; private set; }
@@ -15,5 +16,31 @@ public class Address
     public string City { get; private set; }
     public string State {  get; private set; }
     public string Cep {  get; private set; }
+    public bool IsDeleted {  get; private set; }
     public Donor? Donor { get; private set; }
+
+    public void UpdateStreet(string street)
+    {
+        Street = street;
+    }
+
+    public void UpdateCity(string city)
+    {
+        City = city;
+    }
+
+    public void UpdateState(string state)
+    {
+        State = state;
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+    }
 }
