c1bbfff06ac91b085ca5c88bf760d99cbf99194e
benicio <beniciobrandao@hotmail.com>
2025-04-22 10:52:31 -0300

    Adiciona métodos Delete e Restore para controle de deleção lógica

--- BloodDonationSystem.Core/Entities/Donation.cs
+++ BloodDonationSystem.Core/Entities/Donation.cs
@@ -6,10 +6,22 @@ public class Donation
         DonorId = donorId;
         AmountMl = amountMl;
         DonationDate = DateTime.Now;
+        IsDeleted = false;
     }
     public int Id { get; private set; }
     public int DonorId {  get; private set; }
     public DateTime DonationDate { get; private set; }
     public int AmountMl {  get; private set; }
+    public bool IsDeleted {  get; private set; }
     public Donor? Donor { get; private set; }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+    }
 }
