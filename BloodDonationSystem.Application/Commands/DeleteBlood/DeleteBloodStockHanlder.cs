75342fc3d6f2d12b9fcdc64fe540317868138128
benicio <beniciobrandao@hotmail.com>
2025-04-27 18:25:37 -0300

    refatora

--- BloodDonationSystem.Application/Commands/DeleteBlood/DeleteBloodStockHanlder.cs
+++ BloodDonationSystem.Application/Commands/DeleteBlood/DeleteBloodStockHanlder.cs
@@ -3,7 +3,7 @@ using BloodDonationSystem.Core.Repositories;
 using MediatR;
 
 namespace BloodDonationSystem.Application.Commands.DeleteBlood;
-public class DeleteBloodStockHanlder : IRequestHandler<DeleteBloodStockCommand, ResultViewModel<BloodStockViewModel>>
+public class DeleteBloodStockHanlder : IRequestHandler<DeleteBloodStockCommand, ResultViewModel>
 {
     private readonly IBloodStockRepository _repository;
 
@@ -11,22 +11,15 @@ public class DeleteBloodStockHanlder : IRequestHandler<DeleteBloodStockCommand, 
     {
         _repository = repository;
     }
-    public async Task<ResultViewModel<BloodStockViewModel>> Handle(DeleteBloodStockCommand request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel> Handle(DeleteBloodStockCommand request, CancellationToken cancellationToken)
     {
-        var bloodStock = await _repository.Delete(request.Id);
+        var success = await _repository.Delete(request.Id);
 
-        if (bloodStock is null)
+        if (!success)
         {
             return ResultViewModel<BloodStockViewModel>.Error($"Estoque de sanque com ID {request.Id} não encontrado.");
         }
 
-        if (bloodStock.IsDeleted)
-        {
-            return ResultViewModel<BloodStockViewModel>.Error("Estoque foi excluído.");
-        }
-
-        var model = BloodStockViewModel.FromEntity(bloodStock);
-
-        return ResultViewModel<BloodStockViewModel>.Success(model);
+        return ResultViewModel.Success();
     }
 }
