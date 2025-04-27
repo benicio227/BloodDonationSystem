716a0c3c5fef59e4daea16775feb6e06610863d7
benicio <beniciobrandao@hotmail.com>
2025-04-27 13:36:58 -0300

    Adiciona teste de unidade para DonationTests

---
+++ BloodDonationSystem.UnitTests/Core/DonationTests.cs
@@ -0,0 +1,49 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.UnitTests.Core;
+public class DonationTests
+{
+    [Fact]
+    public void Donation_ShouldBeCreatedWithCorrectValues()
+    {
+
+        var donorId = 1;
+        var amountMl = 470;
+
+
+        var donation = new Donation(donorId, amountMl);
+
+
+        Assert.Equal(donorId, donation.DonorId);
+        Assert.Equal(amountMl, donation.AmountMl);
+     
+    }
+
+    [Fact]
+    public void Delete_ShouldBeSetIsDeletedTrue()
+    {
+
+        var donation = DonationPropertiesTest();
+
+
+        donation.Delete();
+
+
+        Assert.True(donation.IsDeleted);
+    }
+
+    [Fact]
+    public void Restore_ShouldBeSetDeletedFalse()
+    {
+        var donation = DonationPropertiesTest();
+
+        donation.Restore();
+
+        Assert.False(donation.IsDeleted);
+    }
+
+    public Donation DonationPropertiesTest()
+    {
+        return new Donation(1, 470);
+    }
+}
