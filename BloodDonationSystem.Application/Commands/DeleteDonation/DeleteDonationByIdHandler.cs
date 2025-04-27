e9074137da20f1bcd31e73f8dfaca83d31701a2b
benicio <beniciobrandao@hotmail.com>
2025-04-27 18:08:06 -0300

    Refatora para não retornar o objeto deletado ao usuário

--- BloodDonationSystem.Application/Commands/DeleteDonation/DeleteDonationByIdHandler.cs
+++ BloodDonationSystem.Application/Commands/DeleteDonation/DeleteDonationByIdHandler.cs
@@ -3,7 +3,7 @@ using BloodDonationSystem.Core.Repositories;
 using MediatR;
 
 namespace BloodDonationSystem.Application.Commands.DeleteDonation;
-public class DeleteDonationByIdHandler : IRequestHandler<DeleteDonationByIdQuery, ResultViewModel<DonationViewModel>>
+public class DeleteDonationByIdHandler : IRequestHandler<DeleteDonationByIdQuery, ResultViewModel>
 {
     private readonly IDonationRepository _repository;
 
@@ -11,17 +11,15 @@ public class DeleteDonationByIdHandler : IRequestHandler<DeleteDonationByIdQuery
     {
         _repository = repository;
     }
-    public async Task<ResultViewModel<DonationViewModel>> Handle(DeleteDonationByIdQuery request, CancellationToken cancellationToken)
+    public async Task<ResultViewModel> Handle(DeleteDonationByIdQuery request, CancellationToken cancellationToken)
     {
-        var donation = await _repository.Delete(request.Id);
+        var success = await _repository.Delete(request.Id);
 
-        if (donation is null)
+        if (!success)
         {
             return ResultViewModel<DonationViewModel>.Error($"Doação com ID {request.Id} não encontrado.");
         }
 
-        var model = DonationViewModel.FromEntity(donation);
-
-        return ResultViewModel<DonationViewModel>.Success(model);
+        return ResultViewModel.Success();
     }
 }
