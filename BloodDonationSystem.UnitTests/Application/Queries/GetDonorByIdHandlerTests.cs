d2181c05291de91d68d73b3329892ef29d70e69d
benicio <beniciobrandao@hotmail.com>
2025-04-27 09:15:03 -0300

    Cria a pasta Queries para organizar classes de leitura

---
+++ BloodDonationSystem.UnitTests/Application/Queries/GetDonorByIdHandlerTests.cs
@@ -0,0 +1,88 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Queries.GetDonorById;
+using BloodDonationSystem.Core.Repositories;
+using NSubstitute;
+
+namespace BloodDonationSystem.UnitTests.Application.Queries;
+public class GetDonorByIdHandlerTests
+{
+    [Fact]
+    public async Task GetDonorById_ReturnsSuccess_WhenDonorIsFound()
+    {
+        var repository = Substitute.For<IDonorRepository>();
+
+        var donor = new Donor(
+            "Benício Brandão",
+            "beniciobrandao@hotmail.com",
+            new DateTime(1994, 4, 7),
+            "Masculino",
+            57,
+            "A+",
+            "R-"
+        );
+
+        repository.GetById(Arg.Any<int>()).Returns(donor);
+
+        var handler = new GetDonorByIdHandler(repository);
+
+        var command = new GetDonorByIdQuery(1);
+
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Data);
+        Assert.Equal(donor.FullName, result.Data?.FullName);
+    }
+
+    [Fact]
+    public async Task GetDonorById_ReturnsError_WhenDonorNotFound()
+    {
+       
+        var repository = Substitute.For<IDonorRepository>();
+
+       
+        repository.GetById(Arg.Any<int>()).Returns((Donor?)null);
+
+        var handler = new GetDonorByIdHandler(repository);
+        var command = new GetDonorByIdQuery(1);
+
+       
+        var result = await handler.Handle(command, CancellationToken.None);
+
+    
+        Assert.False(result.IsSuccess);
+        Assert.Equal("Doador com ID 1 não encontrado.", result.Message);
+    }
+
+    [Fact]
+    public async Task GetDonorById_ReturnsError_WhenDonorIsDeleted()
+    {
+        // Arrange
+        var repository = Substitute.For<IDonorRepository>();
+        var donor = new Donor(
+            "Benício Brandão",
+            "beniciobrandao@hotmail.com",
+            new DateTime(1994, 4, 7),
+            "Masculino",
+            57,
+            "A+",
+            "R-"
+        );
+
+
+        typeof(Donor).GetProperty("IsDeleted")?.SetValue(donor, true);
+
+        // Configurando o repositório para retornar o doador excluído
+        repository.GetById(Arg.Any<int>()).Returns(donor);
+
+        var handler = new GetDonorByIdHandler(repository);
+        var command = new GetDonorByIdQuery(1); // ID de exemplo
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.False(result.IsSuccess);
+        Assert.Equal("Este doador foi excluído.", result.Message);
+    }
+}
