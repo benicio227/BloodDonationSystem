7e1937f51be4ea30a8a563c3220d009c9790a8f6
benicio <beniciobrandao@hotmail.com>
2025-04-28 12:52:28 -0300

    Cria pasta para registrar classe que recebe o evento

---
+++ BloodDonationSystem.Application/EventHandlers/BloodStockBelowMinimumDomainEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using BloodDonationSystem.Core.Events;
+using MediatR;
+
+namespace BloodDonationSystem.Application.EventHandlers;
+public class BloodStockBelowMinimumDomainEventHandler : INotificationHandler<BloodStockBelowMinimumDomainEvent>
+{
+    public Task Handle(BloodStockBelowMinimumDomainEvent notification, CancellationToken cancellationToken)
+    {
+
+        Console.WriteLine($"⚠️ Alerta: Estoque de {notification.BloodType}{notification.RgFactor} abaixo do mínimo. Atual: {notification.AmountMl}ml / Mínimo: {notification.MinimumAmountMl}ml.");
+
+        return Task.CompletedTask;
+    }
+}
