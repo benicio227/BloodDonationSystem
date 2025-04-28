44c7eec49df15e666ed057530d3fad64cf86a02e
benicio <beniciobrandao@hotmail.com>
2025-04-28 06:44:53 -0300

    Adicion DonationViewModel

--- BloodDonationSystem.Application/Queries/GetAllDonationById/GetAllDonationQuery.cs
+++ BloodDonationSystem.Application/Queries/GetAllDonationById/GetAllDonationQuery.cs
@@ -3,7 +3,7 @@ using BloodDonationSystem.Application.Models;
 using MediatR;
 
 namespace BloodDonationSystem.Application.Queries.GetAllDonationById;
-public class GetAllDonationQuery : IRequest<ResultViewModel<List<Donation>>>
+public class GetAllDonationQuery : IRequest<ResultViewModel<List<DonationViewModel>>>
 {
     public GetAllDonationQuery(int id)
     {
