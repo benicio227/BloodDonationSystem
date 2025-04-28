b894d300b6320a624a25f29fb4ad56fcd868234f
benicio <beniciobrandao@hotmail.com>
2025-04-28 12:51:33 -0300

    Cria pasta Events para registrar as entidades relacionadas aos eventos

---
+++ BloodDonationSystem.Core/Events/BloodStockBelowMinimumDomainEvent.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace BloodDonationSystem.Core.Events;
+public class BloodStockBelowMinimumDomainEvent : INotification
+{
+    public string BloodType { get; }
+    public string RgFactor { get; }
+    public int AmountMl { get; }
+    public int MinimumAmountMl { get; }
+
+    public BloodStockBelowMinimumDomainEvent(string bloodType, string rgFactor, int amountMl, int minimumAmountMl)
+    {
+        BloodType = bloodType;
+        RgFactor = rgFactor;
+        AmountMl = amountMl;
+        MinimumAmountMl = minimumAmountMl;
+    }
+}
