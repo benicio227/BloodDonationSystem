347caefc5e00fef5f41cf4f636df38929407df92
benicio <beniciobrandao@hotmail.com>
2025-05-02 16:59:49 -0300

    Cria pasta InsertUser

---
+++ BloodDonationSystem.Application/Commands/InsertUser/InsertUserCommand.cs
@@ -0,0 +1,23 @@
+﻿using BloodDonationSystem.Application.Models;
+using BloodDonationSystem.Core.Entities;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertUser;
+public class InsertUserCommand : IRequest<ResultViewModel<UserViewModel>>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; } = "Donor";
+    public string FullName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Gender { get; set; }
+    public double Weight { get; set; }
+    public string BloodType { get; set; }
+    public string RgFactor { get; set; }
+
+
+    public User ToEntity()
+    {
+        return new User(Email, Password, Role);
+    }
+}
---
+++ BloodDonationSystem.Application/Commands/InsertUser/InsertUserHandler.cs
@@ -0,0 +1,22 @@
+﻿using BloodDonationSystem.Application.Entities;
+using BloodDonationSystem.Application.Models;
+using MediatR;
+
+namespace BloodDonationSystem.Application.Commands.InsertUser;
+public class InsertUserHandler : IRequestHandler<InsertUserCommand, ResultViewModel<UserViewModel>>
+{
+    public Task<ResultViewModel<UserViewModel>> Handle(InsertUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = request.ToEntity();
+
+        var donor = new Donor(
+             request.FullName,
+             request.BirthDate,
+             request.Gender,
+             request.Weight,
+             request.BloodType,
+             request.RgFactor,
+             user.Id
+        );
+    }
+}
