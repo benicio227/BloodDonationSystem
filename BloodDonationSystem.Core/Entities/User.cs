7d443c1556bdc072a8596f462781c27e1942c9ed
benicio <beniciobrandao@hotmail.com>
2025-05-02 10:18:29 -0300

    Cria entidade User para cadastrar um usuário no sistema

---
+++ BloodDonationSystem.Core/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿namespace BloodDonationSystem.Core.Entities;
+public class User
+{
+    public User(int id, string email, string password, string role)
+    {
+        Id = id;
+        Email = email;
+        Password = password;
+        Role = role;
+        IsDeleted = false;
+    }
+
+    public int Id { get; private set; }
+    public string Email {  get; private set; }
+    public string Password {  get; private set; }
+    public string Role { get; private set; } = "Donor";
+    public bool IsDeleted { get; private set; }
+
+    public void UpdateEmail(string email)
+    {
+        Email = email;
+    }
+
+    public void UpdatePassword(string password)
+    {
+        Password = password;
+    }
+
+
+}
