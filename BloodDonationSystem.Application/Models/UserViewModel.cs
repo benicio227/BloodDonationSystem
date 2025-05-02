f450f12c466890f38878663cda7037ecec00fcfa
benicio <beniciobrandao@hotmail.com>
2025-05-02 17:00:37 -0300

    Cria UserViewModel para apresentar os dados do Usuário

---
+++ BloodDonationSystem.Application/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BloodDonationSystem.Application.Entities;
+
+namespace BloodDonationSystem.Application.Models;
+public class UserViewModel
+{
+    public UserViewModel(string fullName, DateTime birthDate, string gender, double weight, string bloodType, string rgFactor)
+    {
+        FullName = fullName;
+        BirthDate = birthDate;
+        Gender = gender;
+        Weight = weight;
+        BloodType = bloodType;
+        RgFactor = rgFactor;
+    }
+    public string FullName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Gender { get; set; }
+    public double Weight { get; set; }
+    public string BloodType { get; set; }
+    public string RgFactor { get; set; }
+
+    public static UserViewModel FromEntity(Donor donor)
+    {
+        return new UserViewModel(donor.FullName, donor.BirthDate, donor.Gender, donor.Weight, donor.BloodType, donor.RgFactor);
+    }
+}
