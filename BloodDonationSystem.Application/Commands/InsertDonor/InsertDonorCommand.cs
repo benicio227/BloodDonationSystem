ca423c8aa29e32ab65834cbb92efd4d3768e7639
benicio <beniciobrandao@hotmail.com>
2025-05-05 07:29:23 -0300

    Adiciona IMediator, IBrevoEmailService

--- BloodDonationSystem.Application/Commands/InsertDonor/InsertDonorCommand.cs
+++ BloodDonationSystem.Application/Commands/InsertDonor/InsertDonorCommand.cs
@@ -7,6 +7,7 @@ public class InsertDonorCommand : IRequest<ResultViewModel<DonorViewModel>>
 {
     public string FullName { get; set; }
     public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
     public string Gender { get; set; }
     public double Weight { get; set; }
     public string BloodType { get; set; }
@@ -15,6 +16,6 @@ public class InsertDonorCommand : IRequest<ResultViewModel<DonorViewModel>>
 
     public Donor ToEntity()
     {
-        return new Donor(FullName, BirthDate, Gender, Weight, BloodType, RgFactor, UserId);
+        return new Donor(FullName, Email, BirthDate, Gender, Weight, BloodType, RgFactor, UserId);
     }
 }
