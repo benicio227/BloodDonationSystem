8685c3ad5a2beba64130e8f1e6d84b30d3c825e7
Benicio Brandao <beniciobrandao@hotmail.com>
2025-06-26 09:24:26 -0300

    Inicializa as propriedades para evitar que o compilador reclame da nulidade

--- BloodDonationSystem.Application/Commands/InsertUser/InsertUserCommand.cs
+++ BloodDonationSystem.Application/Commands/InsertUser/InsertUserCommand.cs
@@ -6,10 +6,10 @@ using MediatR;
 namespace BloodDonationSystem.Application.Commands.InsertUser;
 public class InsertUserCommand : IRequest<ResultViewModel<UserViewModel>>
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public UserRole Role { get; set; }
-    public string FullName { get; set; }
+    public string FullName { get; set; } = string.Empty;
     public DateTime BirthDate { get; set; }
     public Gender Gender { get; set; }
     public double Weight { get; set; }
